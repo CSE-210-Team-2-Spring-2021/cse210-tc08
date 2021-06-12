31950c1651a9abdd719c9740875a35acfd8dc3e5
drbubbles40-school <82907102+drbubbles40-school@users.noreply.github.com>
2021-06-12 17:58:38 -0600

    bricks now restore

--- batter/game/bricks.py
+++ batter/game/bricks.py
@@ -20,7 +20,7 @@ class Bricks(Actor):
         """
         super().__init__()
 
-    def destroyBricks(self,brick_position,cast):
+    def destroy_bricks(self,brick_position,cast):
         """Deletes the brick where the ball hits (The ball is in the same place as a brick )
         
         Args:
@@ -31,18 +31,18 @@ class Bricks(Actor):
         bricks.remove(brick_position)         #It's going to remove a brick in the ball's position. The remove() method will remove only the first occurrence.
         return cast
 
-    def generateBricks(self,cast):
+    def generate_bricks(self,cast):
         """Generate a new list of items and append it into "cast"
         
         Args:
             cast (dict):            {key: tag, value: list}.
         """ 
-        bricks = cast["brick"]
+        cast['brick'] = []
+        
         for x in range(5, 75):
             for y in range(2, 6):
                 position = Point(x, y)
-                if bricks.count(position) == 0:             #Check if the Brick's position it empty
-                    self.set_text("*")
-                    self.set_position(position)
-                    bricks.append(self)
-        return cast
+                brick = Actor()
+                brick.set_text("*")
+                brick.set_position(position)
+                cast["brick"].append(brick)
